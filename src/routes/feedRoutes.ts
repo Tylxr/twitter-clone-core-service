195c774c86b210b003fb230cafa332cc67527fd6
Tyler Marshall <tylerjeremiahh@gmail.com>
2024-01-02 20:45:56 +0000

    Removing redundant code

--- src/routes/feedRoutes.ts
+++ src/routes/feedRoutes.ts
@@ -3,14 +3,6 @@ import express from "express";
 
 const router = express.Router();
 
-// POST
-// router.post("/", postTweet);
-
-// // PATCH
-// router.patch("/:tweetId/comment", postComment);
-// router.patch("/:tweetId/like", likeTweet);
-// router.patch("/:tweetId/comment/:commentId/like", likeTweetComment);
-
 // GET
 router.get("/fromAll", getFeedFromAll);
 

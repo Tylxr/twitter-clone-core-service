1ad815bf561069cac4f4ebfdd9bb0cc3c467677e
Tyler Marshall <tylerjeremiahh@gmail.com>
2024-02-18 10:54:42 +0000

    Adding basic cors setup for localhost

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,15 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import models from "./models";
 import routes from "./routes";
+import cors from "cors";
+
+const corsOptions = {
+	origin: "http://localhost:3000", // client
+	credentials: true,
+};
 
 const app = express();
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
